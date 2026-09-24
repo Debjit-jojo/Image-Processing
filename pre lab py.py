3a2904c1592b7870a859bb33d2344395d87466f3
Debjit B. Talukdar <debjittalukdar1@gmail.com>
2026-09-24 10:17:39 +0530

    Change plot title from 'CS24242' to 'CS24202'

--- pre lab py.py
+++ pre lab py.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 img = cv2.imread(r"C:\Users\debji\PycharmProjects\PythonProject1\WhatsApp-Image-2023-03-24-at-14.52.58 (1).jpeg")
-plt.title("CS24242")
+plt.title("CS24202")
 plt.imshow(img)
 plt.waitforbuttonpress()
 plt.close("all")
